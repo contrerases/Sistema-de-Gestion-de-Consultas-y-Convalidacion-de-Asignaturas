8d1b797423f74822f29524e9ed3c517b3f292ca4
contrerases <camilocontrerases@gmail.com>
2024-06-03 13:57:10 -0400

    Form de send request

--- Client/src/modules/student/new_request/NewRequestForm.vue
+++ Client/src/modules/student/new_request/NewRequestForm.vue
@@ -1,6 +1,22 @@
 <template>
+  <SuccessDialog
+    :isOpen="showSuccessDialog"
+    title="Solicitud Enviada"
+    message="La solicitud ha sido enviada correctamente"
+    @close="toggleSuccessDialog" 
+    />
+
+  <AlertDialog
+    :isOpen="showErrorDialog"
+    title="Error"
+    message="No se pudo enviar la solicitud, por favor intenta de nuevo"
+    @close="toggleErrorDialog"
+  />
+
+
   <main>
-    <div
+    
+  <div
       class="bg-input rounded-xl w-full h-[600px] p-10 flex flex-col justify-between"
     >
       <div>
@@ -9,16 +25,34 @@
             Selecciona el tipo de convalidación:
           </h1>
 
-          <Select>
-            <SelectTrigger class="w-1/4">
+          <Select v-model="convalidation_types">
+            <SelectTrigger class="w-1/3">
               <SelectValue placeholder="..." />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectItem value="Libre"> Libre </SelectItem>
-                <SelectItem value="Electivo"> Electivo </SelectItem>
-                <SelectItem value="Electivo de INF">
-                  Electivo de Informatica
+                <SelectItem value="1"> Libre </SelectItem>
+                <SelectItem value="2"> Electivo </SelectItem>
+                <SelectItem value="3"> Electivo de Informatica
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div class="pb-8">
+          <h1 class="text-xl font-mono pb-2">
+            Selecciona el curso del plan de estudios:
+          </h1>
+
+          <Select v-model="id_curriculum_course">
+            <SelectTrigger class="w-1/3">
+              <SelectValue placeholder="..." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem v-for="course in curriculum_courses" :key="course.id" :value="String(course.id)">
+                  {{ course.name }}
                 </SelectItem>
               </SelectGroup>
             </SelectContent>
@@ -30,35 +64,36 @@
             Selecciona el tipo de curso realizado:
           </h1>
 
-          <Select v-model="convalidation_type">
-            <SelectTrigger class="w-1/4">
+          <Select v-model="id_convalidation_type">
+            <SelectTrigger class="w-1/3">
               <SelectValue placeholder="..." />
             </SelectTrigger>
             <SelectContent>
-              <SelectGroup>
-                <SelectItem :value="CourseConvalidationTypes.EXTERNA">
+                <SelectItem value="2" v-if="convalidation_types === '1' || convalidation_types === '2'">
                   {{ CourseConvalidationTypes.EXTERNA }}
                 </SelectItem>
-                <SelectItem :value="CourseConvalidationTypes.TALLER">
+                <SelectItem value="3" v-if="convalidation_types === '1'">
                   {{ CourseConvalidationTypes.TALLER }}
                 </SelectItem>
-                <SelectItem :value="CourseConvalidationTypes.PROYECTO">
+                <SelectItem value="5" v-if="convalidation_types === '1'">
                   {{ CourseConvalidationTypes.PROYECTO }}
                 </SelectItem>
-                <SelectItem :value="CourseConvalidationTypes.CERTIFICADO">
+                <SelectItem value="4" v-if="convalidation_types === '1'">
                   {{ CourseConvalidationTypes.CERTIFICADO }}
                 </SelectItem>
-                <SelectItem :value="CourseConvalidationTypes.INF">
+                <SelectItem value="1" v-if="convalidation_types === '1' || convalidation_types === '2' || convalidation_types === '3'"> 
                   {{ CourseConvalidationTypes.INF }}
                 </SelectItem>
-              </SelectGroup>
             </SelectContent>
           </Select>
         </div>
 
+
+        
+
         <div
           class="pb-8 flex justify-between w-full"
-          v-if="convalidation_type === CourseConvalidationTypes.EXTERNA"
+          v-if="id_convalidation_type === '2'"
         >
           <div>
             <h1 class="text-xl font-mono pb-2">Selecciona el departamento</h1>
@@ -85,8 +120,8 @@
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectItem value="Enviada">
-                    Lista de cursos Externos ...
+                  <SelectItem v-for="subject in subjects" :key="subject.id" :value="String(subject.id)">
+                    {{ subject.name }}
                   </SelectItem>
                 </SelectGroup>
               </SelectContent>
@@ -96,47 +131,53 @@
 
         <div
           class="pb-8"
-          v-if="convalidation_type === CourseConvalidationTypes.INF"
+          v-if="id_convalidation_type === '1'"
         >
           <h1 class="text-xl font-mono pb-2">
             Selecciona la asignatura cursada:
           </h1>
 
-          <Select>
+          <Select v-model="id_subject_to_convalidate">
             <SelectTrigger class="w-1/4">
               <SelectValue placeholder="..." />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="Enviada"> Lista de cursos de INF </SelectItem>
+              <SelectItem v-for="subject in subjects" :key="subject.id" :value="String(subject.id)">
+                {{ subject.name }}
+              </SelectItem>
             </SelectContent>
           </Select>
         </div>
 
         <div
-          class="pb-8 flex justify-between w-full"
-          v-if="convalidation_type === CourseConvalidationTypes.TALLER"
-        >
-          <div>
-            <h1 class="text-xl font-mono pb-2">Ingresa el nombre del curso:</h1>
-            <input type="text" class="bg-input border w-full rounded-lg p-2" />
-          </div>
-          <div>
-            <h1 class="text-xl font-mono pb-2">Subir Certificado (PDF)</h1>
-            <div class="box border rounded-lg p-2">
-              <input type="file" accept=".pdf" @change="handleFileUpload" />
-            </div>
-          </div>
-        </div>
+        class="pb-8"
+        v-if="id_convalidation_type === '3'"
+      >
+        <h1 class="text-xl font-mono pb-2">
+          Selecciona el taller cursado:
+        </h1>
+
+        <Select v-model="id_workshop_to_convalidate">
+          <SelectTrigger class="w-1/3">
+            <SelectValue placeholder="..." />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="workshop in workshops" :key="workshop.id" :value="String(workshop.id)">
+              {{ workshop.name }}
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
 
         <div
           class="pb-8 flex justify-between w-full"
-          v-if="convalidation_type === CourseConvalidationTypes.PROYECTO"
+          v-if="id_convalidation_type === '5'"
         >
           <div>
             <h1 class="text-xl font-mono pb-2">
               Ingresa el nombre del Proyecto:
             </h1>
-            <input type="text" class="bg-input border w-fulls rounded-lg p-2" />
+            <input type="text" class="bg-input border w-full rounded-lg p-2" v-model="personal_project_name" />
           </div>
           <div>
             <h1 class="text-xl font-mono pb-2">Subir archivo (PDF)</h1>
@@ -145,10 +186,33 @@
             </div>
           </div>
         </div>
+
+        
+        <div
+          class="pb-8 flex justify-between w-full"
+          v-if="id_convalidation_type === '4'"
+        >
+
+          <div>
+            <h1 class="text-xl font-mono pb-2">
+              Ingresa el nombre del curso certificado:
+            </h1>
+            <input type="text" class="bg-input border w-full rounded-lg p-2" v-model="certified_course_name" />
+          </div>
+          <div>
+            <h1 class="text-xl font-mono pb-2">Subir Certificado (PDF)</h1>
+            <div class="box border
+            rounded-lg p-2">
+              <input type="file" accept=".pdf" @change="handleFileUpload" />
+            </div>
+          </div>  
+        </div>
       </div>
 
+      
+
       <div class="flex justify-end">
-        <button class="bg-primary py-4 px-8 rounded-xl">
+        <button class="bg-primary py-4 px-8 rounded-xl" @click="sendConvalidation">
           Enviar convalidación
         </button>
       </div>
@@ -171,7 +235,6 @@ import { CourseConvalidationTypes } from "@/enums/courses_convaldiation_types";
 import type {
   ConvalidationResponse,
   ConvalidationBase,
-  ConvalidationPost,
 } from "@/interfaces/convalidation_model";
 import type { CurriculumCourseBase } from "@/interfaces/curriculum_course_model";
 import type { SubjectBase } from "@/interfaces/subject_model";
@@ -184,7 +247,13 @@ import { getAllTypesCourses } from "@/services/type_course_api";
 import { getAllSubject } from "@/services/subject_api";
 import { getAllWorkshops } from "@/services/workshop_api";
 
-const convalidation_type = ref<CourseConvalidationTypes>(CourseConvalidationTypes.VACIO);
+
+import AlertDialog from "@/common/dialogs/AlertDialog.vue";
+import SuccessDialog from "@/common/dialogs/SuccessDialog.vue";
+
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const curriculum_courses = ref<CurriculumCourseBase[]>([]);
 const subjects = ref<SubjectBase[]>([]);
@@ -249,6 +318,7 @@ const id_student = ref<number>(2);
 const id_convalidation_type = ref<string>(
   "Seleccione un tipo de convalidación"
 );
+const convalidation_types = ref<string | null>(null);
 const state = ref<string>("Enviada");
 const comments = ref<string | null>(null);
 const creation_date = ref<string | null>(null);
@@ -287,14 +357,17 @@ function updateConvalidation() {
   };
 }
 
-function sendConvalidation() {
+async function sendConvalidation() {
   updateConvalidation();
 
   try {
-    insertConvalidation(convalidation);
-    window.location.reload();
+    console.log("Convalidación enviada:", convalidation);
+    await insertConvalidation(convalidation);
+    toggleSuccessDialog();
   } catch (error) {
+    toggleErrorDialog();
     console.error("Error al enviar la convalidación:", error);
+    throw error;
   }
 }
 
@@ -304,6 +377,24 @@ function handleFileUpload(event: Event) {
     file_data.value = target.files[0];
   }
 }
+
+function toggleSuccessDialog() {
+  if (showSuccessDialog.value) {
+    showSuccessDialog.value = false;
+    router.push("/estudiante/inicio");
+  } else {
+    showSuccessDialog.value = true;
+  }
+}
+
+const showSuccessDialog = ref<boolean>(false);
+
+function toggleErrorDialog() {
+  showErrorDialog.value = !showErrorDialog.value;
+}
+
+const showErrorDialog = ref<boolean>(false);
+
 </script>
 
 <style scoped lang="postcss"></style>
